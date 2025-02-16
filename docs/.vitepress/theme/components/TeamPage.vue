f047ac9039852d5178ab4a50a8c83ad6fd4a7ae0
root <root@DESKTOP-HK8I2OR.>
2025-02-16 17:08:16 +0530

    feat:add team members and q and a 🙋‍♂️

---
+++ docs/.vitepress/theme/components/TeamPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <VPTeamMembers size="small" :members="members" />
+</template>
+
+<script setup>
+import { VPTeamMembers } from 'vitepress/theme'
+
+const members = [
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/74690352?v=4',
+    name: 'W.P.S Lakshitha',
+    title: 'Co-Founder',
+    links: [
+      { icon: 'github', link: 'https://github.com/wpslakshitha' },
+      { icon: 'twitter', link: 'https://twitter.com/wpslakshitha.eg' }
+    ]
+  },
+    {
+    avatar: 'https://www.github.com/yyx990803.png',
+    name: 'Evan You',
+    title: 'Creator of Vue.js',
+    links: [
+      { icon: 'github', link: 'https://github.com/yyx990803' },
+      { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
+    ]
+  },
+  {
+    avatar: 'https://www.github.com/antfu.png',
+    name: 'Anthony Fu',
+    title: 'Vue.js Core Team',
+    links: [
+      { icon: 'github', link: 'https://github.com/antfu' },
+      { icon: 'twitter', link: 'https://twitter.com/antfu7' }
+    ]
+  },
+]
+</script>
